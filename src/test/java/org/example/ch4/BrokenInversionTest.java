8030115d727a5a58ad6fd16dbb8b73335d3ef746
progmonster <progmonster@gmail.com>
2022-09-04 18:11:22 +0300

    Chapter 4. Orphan removal

--- src/test/java/org/example/ch4/BrokenInversionTest.java
+++ src/test/java/org/example/ch4/BrokenInversionTest.java
@@ -1,12 +1,17 @@
 package org.example.ch4;
 
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
+import java.util.List;
+
 import static org.example.DatabaseUtils.openSession;
 import static org.example.DatabaseUtils.reinitializeDatabase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BrokenInversionTest {
@@ -23,10 +28,17 @@ public class BrokenInversionTest {
 
         message.setEmail(email);
 
-        try(Session session = openSession()) {
+        Message message2 = new Message("BrokenMessage2");
+
+        message2.setEmail(email);
+
+
+
+        try (Session session = openSession()) {
             session.beginTransaction();
 
             session.persist(message);
+            session.persist(message2);
             session.persist(email);
 
             session.getTransaction().commit();
@@ -35,16 +47,35 @@ public class BrokenInversionTest {
         assertNotNull(email.getMessages());
         assertNotNull(message.getEmail());
 
-        try(Session session = openSession()) {
+        try (Session session = openSession()) {
             email = session.get(Email.class, email.getId());
-            message = session.get(Message.class, message.getId());
 
-            System.out.println(email);
-            System.out.println(message);
+            assertNotNull(message.getEmail());
+            assertEquals(2, email.getMessages().size());
         }
 
-        assertNotNull(email.getMessages());
-        assertNotNull(message.getEmail());
+
+        try (Session session = openSession()) {
+            session.beginTransaction();
+
+            email = session.get(Email.class, email.getId());
+
+            Iterator<Message> messageIter = email.getMessages().iterator();
+
+            messageIter.next();
+
+            messageIter.remove();
+
+            session.getTransaction().commit();
+        }
+
+        try (Session session = openSession()) {
+            List<Message> messages = session
+                    .createQuery("from Message m", Message.class)
+                    .list();
+
+            assertEquals(1, messages.size());
+        }
     }
 
     @AfterEach
