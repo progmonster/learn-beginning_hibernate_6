2c03bfbe4d2ef96c230ede337c08fb0936382eba
progmonster <progmonster@gmail.com>
2022-09-04 19:55:19 +0300

    Chapter 4. Orphan removal

--- src/test/java/org/example/ch4/OrphanRemovalTest.java
+++ src/test/java/org/example/ch4/OrphanRemovalTest.java
@@ -1,7 +1,6 @@
 package org.example.ch4;
 
 import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,19 +8,18 @@ import org.junit.jupiter.api.Test;
 import java.util.Iterator;
 import java.util.List;
 
-import static org.example.DatabaseUtils.openSession;
-import static org.example.DatabaseUtils.reinitializeDatabase;
+import static org.example.DatabaseUtils.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class BrokenInversionTest {
+public class OrphanRemovalTest {
     @BeforeEach
     void setUp() {
         reinitializeDatabase();
     }
 
     @Test
-    void testBrokenInversionCode() {
+    void testRemoveOrphan() {
         Message message = new Message("BrokenMessage");
 
         Email email = new Email("BrokenSubject");
@@ -32,6 +30,61 @@ public class BrokenInversionTest {
 
         message2.setEmail(email);
 
+
+        try (Session session = openSession()) {
+            session.beginTransaction();
+
+            session.persist(message);
+            session.persist(message2);
+            session.persist(email);
+
+            session.getTransaction().commit();
+        }
+
+        assertNotNull(email.getMessages());
+        assertNotNull(message.getEmail());
+
+        try (Session session = openSession()) {
+            email = session.get(Email.class, email.getId());
+
+            assertNotNull(message.getEmail());
+            assertEquals(2, email.getMessages().size());
+        }
+
+        try (Session session = openSession()) {
+            session.beginTransaction();
+
+            email = session.get(Email.class, email.getId());
+
+            Iterator<Message> messageIter = email.getMessages().iterator();
+
+            messageIter.next();
+
+            messageIter.remove();
+
+            session.getTransaction().commit();
+        }
+
+        try (Session session = openSession()) {
+            List<Message> messages = session
+                    .createQuery("from Message m", Message.class)
+                    .list();
+
+            assertEquals(1, messages.size());
+        }
+    }
+
+    @Test
+    void testRemoveOrphans() {
+        Message message = new Message("BrokenMessage");
+
+        Email email = new Email("BrokenSubject");
+
+        message.setEmail(email);
+
+        Message message2 = new Message("BrokenMessage2");
+
+        message2.setEmail(email);
 
 
         try (Session session = openSession()) {
@@ -60,11 +113,7 @@ public class BrokenInversionTest {
 
             email = session.get(Email.class, email.getId());
 
-            Iterator<Message> messageIter = email.getMessages().iterator();
-
-            messageIter.next();
-
-            messageIter.remove();
+            session.remove(email);
 
             session.getTransaction().commit();
         }
@@ -74,12 +123,12 @@ public class BrokenInversionTest {
                     .createQuery("from Message m", Message.class)
                     .list();
 
-            assertEquals(1, messages.size());
+            assertEquals(0, messages.size());
         }
     }
 
     @AfterEach
     void tearDown() {
-        //uninitializeDatabase();
+        uninitializeDatabase();
     }
 }
