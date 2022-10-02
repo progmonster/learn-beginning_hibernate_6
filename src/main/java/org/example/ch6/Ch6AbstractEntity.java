39202f4275d7b2f37ffa68921945b4e020b13a40
Alexei Katorgin <progmonster@gmail.com>
2022-10-02 14:26:31 +0300

    Ch6. MappedSuperclass, updated, created timestamps

---
+++ src/main/java/org/example/ch6/Ch6AbstractEntity.java
@@ -0,0 +1,52 @@
+package org.example.ch6;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Data
+@MappedSuperclass
+public class Ch6AbstractEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    private Instant created;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private Instant updated;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getCreated() {
+        return created;
+    }
+
+    public void setCreated(Instant created) {
+        this.created = created;
+    }
+
+    public Instant getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Instant updated) {
+        this.updated = updated;
+    }
+}
---
+++ src/test/java/org/example/ch6/MappedSuperclassTest.java
@@ -0,0 +1,41 @@
+package org.example.ch6;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.example.DatabaseUtils.openSession;
+import static org.example.DatabaseUtils.reinitializeDatabase;
+
+public class MappedSuperclassTest {
+    @BeforeEach
+    void setUp() {
+        reinitializeDatabase();
+    }
+
+    @Test
+    void testMappedSuperclassAndUpdateCreateTimestamps() throws Exception {
+        Ch6DerivedEntity entity;
+
+        try (var session = openSession()) {
+            session.beginTransaction();
+
+            entity = new Ch6DerivedEntity("title1");
+
+            session.persist(entity);
+
+            session.getTransaction().commit();
+        }
+
+        Thread.sleep(2000);
+
+        try (var session = openSession()) {
+            session.beginTransaction();
+
+            entity = session.get(Ch6DerivedEntity.class, entity.getId());
+
+            entity.setTitle("updatedTitle");
+
+            session.getTransaction().commit();
+        }
+    }
+}
