fd089bbfc215b2765772205ce7582b9e23c441f2
Alexei Katorgin <progmonster@gmail.com>
2022-10-02 07:47:20 +0300

    Ch6. Lobs

---
+++ src/main/java/org/example/ch6/Ch6LobEntity.java
@@ -0,0 +1,37 @@
+package org.example.ch6;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Ch6LobEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Lob
+    private String textContent;
+
+    @Lob
+    @Basic(fetch = LAZY)
+    private byte[] primitiveByteContent;
+
+    @Lob
+    @Basic(fetch = LAZY)
+    private Byte[] byteContent;
+
+    public Ch6LobEntity(String textContent, byte[] primitiveByteContent, Byte[] byteContent) {
+        this.textContent = textContent;
+        this.primitiveByteContent = primitiveByteContent;
+        this.byteContent = byteContent;
+    }
+}
---
+++ src/test/java/org/example/ch6/LobTest.java
@@ -0,0 +1,32 @@
+package org.example.ch6;
+
+import org.hibernate.Session;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.example.DatabaseUtils.openSession;
+import static org.example.DatabaseUtils.reinitializeDatabase;
+
+public class LobTest {
+    @BeforeEach
+    void setUp() {
+        reinitializeDatabase();
+    }
+
+    @Test
+    void testLob() {
+        try (Session session = openSession()) {
+            session.beginTransaction();
+
+            Ch6LobEntity lobEntity = new Ch6LobEntity(
+                    "text content",
+                    new byte[] {1, 2, 3},
+                    new Byte[] {4, 5, 6}
+            );
+
+            session.persist(lobEntity);
+
+            session.getTransaction().commit();
+        }
+    }
+}
